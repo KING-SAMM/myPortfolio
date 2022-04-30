0c8da9fe8be1cecba1439b3591fd8b1c16760bba
KING-Samm <kcsamm11@gmail.com>
2022-04-30 22:59:00 +0100

    Added Navbar component to setup

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { Home, About, Projects, Skills, Contact, Footer } from './containers';
+import { Navbar } from './components';
 
 const App = () => {
 
   return (
     <div className="App">
+      <Navbar />
       <Home />
       <About />
       <Projects />
