4a2fe159935c110d730e3fd7388b5ffa48fe8b5c
KING-Samm <kcsamm11@gmail.com>
2022-05-06 22:56:34 +0100

    Modified Home and Kayout styling

--- frontend_react/src/containers/Home/Home.jsx
+++ frontend_react/src/containers/Home/Home.jsx
@@ -6,8 +6,9 @@ import './Home.scss';
 const Home = () => {
   return (
     <div className="container home-page">
-      <div className="text-zone">
-        <h1>Hi, <br /> I'm
+      <div className="main-text">
+        <h1 className="text-blue-100 text-[50px] m-0">
+          Hi, <br /> I'm
           <img src={ logoTitle } alt="description" />
           amm
           <br />
