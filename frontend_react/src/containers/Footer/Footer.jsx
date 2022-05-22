ca93e43599b7613699bed9e121f67f4160567171
kcsamm <kcsamm11@gmail.com>
2022-05-22 22:41:18 +0100

    Updated scroll positions of contents and backgrounds for small and medium screens

--- frontend_react/src/containers/Footer/Footer.jsx
+++ frontend_react/src/containers/Footer/Footer.jsx
@@ -4,8 +4,10 @@ import './footer.scss';
 const Footer = () => {
   return (
     <div className='container footer-section'>
-      <Social socialClass="social__mobile" />
-      <Copyright copyClass="copy__mobile" />
+      <div className='flex justify-center'>
+        <Social socialClass="social__mobile" />
+        <Copyright copyClass="copy__mobile" />
+      </div>
     </div>
   )
 }
